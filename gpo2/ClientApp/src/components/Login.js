b1f34404da4b3552f4fa9899a5222d1297298a6a
tanestila <millafko@gmail.com>
2018-10-09 20:26:30 +0700

    add query in component

--- gpo2/ClientApp/src/components/Login.js
+++ gpo2/ClientApp/src/components/Login.js
@@ -1,16 +1,48 @@
 ﻿import React, { Component } from 'react';
-
+// TODO: Добавить bootstrap по-человечески
 export class Login extends Component {
     displayName = Login.name
 
+    constructor(props) {
+        super(props);
+        this.state =
+        {
+            password: "",
+            login: "",
+            result: ""
+        };
+    }
+
+    //Test
+    handleClick() {
+        fetch('api/Auth/LoginTest')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            });
+    }
+    // Post query, выдает 404 FIXIT
+    Login = () => {
+        fetch('api/Auth/Login', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstParam: 'yourValue'
+            })
+        });
+    }
+
     render() {
         return (
-            <form role="form">
+            <form >
                 <div className="form-group">
-                    <input type="text" placeholder="Username" />
-                    <input type="password"  placeholder="Password" />
+                    <input type="text" placeholder="Username" className="form-control" />
+                    <input type="password" placeholder="Password" className="form-control" />
                 </div>
-                <button type="submit">Submit</button>
+                <button type="button" className="btn btn-primary" onClick={this.Login}>Submit</button>
             </form >
         );
     }
---
+++ gpo2/Controllers/AuthController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gpo2.Controllers
+{
+
+    // Можешь создать новый контролер 
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        [HttpGet("[action]")]
+        public JsonResult LoginTest() //test
+        {
+            
+            return Json("Success");
+        }
+
+        // GET: Auth/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Auth/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Auth/Login
+        [HttpPost]
+        public JsonResult Login() // Должно работать, но не работает
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return Json("Success");
+            }
+            catch
+            {
+                return Json("Error"); ;
+            }
+        }
+
+        // GET: Auth/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Auth/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Login));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Auth/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Auth/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Login));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
