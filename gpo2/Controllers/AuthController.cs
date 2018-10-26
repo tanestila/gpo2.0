4085b91d87c7fd4eed93f0bec7e2fde61878af56
Павел Петров <pashka499@yandex.ru>
2018-10-26 16:07:10 +0700

    correct SeedData

--- gpo2/Controllers/AuthController.cs
+++ gpo2/Controllers/AuthController.cs
@@ -41,8 +41,9 @@ namespace gpo2.Controllers
                 string certstr = cert.InnerText;
                 certstr = certstr.Trim();
                 X509Certificate2 certinfo = new X509Certificate2(Convert.FromBase64String(certstr));
+                //if (_userContext.Cert
                 return Json("Success");
-                certinfo.
+                
             }
             catch (Exception e)
             {
--- gpo2/Models/UserContext.cs
+++ gpo2/Models/UserContext.cs
@@ -13,6 +13,7 @@ namespace gpo2.Models
         {
         }
         public DbSet<User> Users { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
 }
     public static class SeedData
     {
@@ -22,18 +23,31 @@ namespace gpo2.Models
                 serviceProvider.GetRequiredService<DbContextOptions<UserContext>>()))
             {
                 // Look for any movies.
-                if (context.Users.Any())
+                if (context.Users.Any() && context.Certificates.Any())
                 {
                     return;   // DB has been seeded
                 }
-
-                context.Users.Add(
+                if (!context.Users.Any())
+                    context.Users.Add(
                      new User
                      {
                          login = "admin",
                          password = "admin"
                      }
                 );
+                if (!context.Certificates.Any())
+                    context.Certificates.Add(
+                        new Certificate
+                        {
+                            userid = 1,
+                            serialnumber = "123456QWERTY",
+                            thumbprint = "tyktyk",
+                            datefrom = new DateTime(1997, 1, 1),
+                            dateto = new DateTime(2099, 1, 1),
+                            actual = true,
+                            valid = true,
+                            subject = "gpoCA"
+                        });
                 context.SaveChanges();
             }
         }
