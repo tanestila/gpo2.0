47d920687a984e22f49a24a89414c95255bf67e4
aparshakova <millafko@gmail.com>
2018-10-29 17:04:35 +0700

    я думаю идея ливнуть с реакта уже не такая плохая

--- gpo2/ClientApp/src/components/LoginWithCertificate.js
+++ gpo2/ClientApp/src/components/LoginWithCertificate.js
@@ -13,12 +13,6 @@ export class LoginWithCertificate extends Component {
                 
             };
     }
-    componentWillMount() {
-        const script = document.createElement("script");
-
-        script.src = "/Scripts/code.js";
-        document.body.appendChild(script);
-    }
 
     componentDidMount() {
        
@@ -35,31 +29,6 @@ export class LoginWithCertificate extends Component {
                 
                
                 
-                <p id="info_msg" name="CertificateTitle">Сертификат:</p>
-                <div id="item_border" name="CertListBoxToHide">
-                    <select size="4" name="CertListBox" id="CertListBox" > </select>
-                </div>
-
-                <div id="boxdiv" >
-                <span id="errorarea">
-                    У вас отсутствуют личные сертификаты. Вы можете получить сертификат от тестового УЦ, предварительно установив корневой сертификат тестового УЦ в доверенные.
-                            </span>
-            </div>
-
-
-            <div id="cert_info">
-                <h3 id="cert_txt" >Информация о сертификате</h3>
-                    <p className="info_field" id="subject"></p>
-                    <p className="info_field" id="issuer"></p>
-                    <p className="info_field" id="from"></p>
-                    <p className="info_field" id="till"></p>
-                    <p className="info_field" id="provname"></p>
-                    <p className="info_field" id="algorithm"></p>
-                    <p className="info_field" id="status"></p>
-                </div>
-                <script language="javascript">
-                    CheckForPlugIn('isPlugInEnabled');
-                            </script>
                 </div>
         );
     }
